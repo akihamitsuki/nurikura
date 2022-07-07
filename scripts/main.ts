c8792be2f2a0d424650228988d307e2ae62d14f2
akihamitsuki <46617616+akihamitsuki@users.noreply.github.com>
2022-07-07 14:48:31 +0900

    記述を整理

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,11 +1,11 @@
 import * as mc from 'mojang-minecraft';
 import { Player } from './Player';
 import { Score } from './Score';
-import { setColor, projectileHit } from './paint';
 import { Setting } from './Setting';
+import { setColor, onProjectileHit, reloadBullet } from './paint';
 
 mc.world.events.beforeItemUse.subscribe(setColor);
-mc.world.events.projectileHit.subscribe(projectileHit);
+mc.world.events.projectileHit.subscribe(onProjectileHit);
 
 mc.world.events.playerJoin.subscribe((event) => {
   // 独自のプレイヤークラスを生成する
@@ -40,38 +40,3 @@ mc.world.events.tick.subscribe((event) => {
     reloadBullet();
   }
 });
-
-/**
- * 弾を補充する
- */
-function reloadBullet() {
-  const snowball = new mc.ItemStack(mc.MinecraftItemTypes.snowball, 1);
-  // すべてのプレイヤーで繰り返し
-  for (const mcPlayer of mc.world.getPlayers()) {
-    // スニークをしているかを判定
-    if (!mcPlayer.isSneaking) {
-      continue;
-    }
-    const player = new Player(mcPlayer);
-    // 足元のブロックを確認
-    const bottomBlock = player.getBottomBlock();
-    if (!bottomBlock.isSame(mc.MinecraftBlockTypes.wool)) {
-      continue;
-    }
-    // そのプレイヤーの色と一致するか
-    const playerColor = player.getColorName();
-    if (playerColor && playerColor === bottomBlock.getColor()) {
-      // そのプレイヤーの座標にアイテムを作成(すぐに取得される)
-      mcPlayer.dimension.spawnItem(snowball, mcPlayer.location);
-    }
-  }
-}
-
-/** ブロックを壊したら雪玉 */
-// mc.world.events.blockBreak.subscribe((event) => {
-//   overworld.runCommand(`say ${event.brokenBlockPermutation.type.id}`);
-//   if (event.brokenBlockPermutation.type.id === mc.MinecraftBlockTypes.wool.id) {
-//     const snowball = new mc.ItemStack(mc.MinecraftItemTypes.snowball, 16);
-//     event.block.dimension.spawnItem(snowball, event.block.location);
-//   }
-// });
--- scripts/paint.ts
+++ scripts/paint.ts
@@ -35,24 +35,15 @@ export function setColor(event: mc.BeforeItemUseEvent): void {
   event.cancel = true;
 }
 
-// interface ColorCount {
-//   color: string;
-//   count: number;
-// }
-
-// function getColorCountObject() {
-//   const colorCount: ColorCount[] = [];
-//   for (const setting of colorSetting) {
-//     colorCount.push({
-//       color: setting.id,
-//       count: 0,
-//     });
-//   }
-//   return colorCount;
-// }
-
-function getPaintPermutation(paintBlockType: mc.BlockType, colorName: string): mc.BlockPermutation {
-  const paintPermutation = paintBlockType.createDefaultBlockPermutation();
+/**
+ * ブロック情報を作成
+ *
+ * @param blockType
+ * @param colorName
+ * @returns
+ */
+function createPaintPermutation(blockType: mc.BlockType, colorName: string): mc.BlockPermutation {
+  const paintPermutation = blockType.createDefaultBlockPermutation();
   // 色プロパティを取得する
   const colorProperty = paintPermutation.getProperty(mc.BlockProperties.color) as mc.StringBlockProperty;
   // 色を設定する
@@ -62,12 +53,81 @@ function getPaintPermutation(paintBlockType: mc.BlockType, colorName: string): m
 }
 
 /**
+ * ブロックを塗る
+ *
+ * @param hitBlock
+ * @param colorName
+ */
+function paintBlock(hitBlock: mc.Block, colorName: string) {
+  // 半径
+  const radius = 1;
+  // 塗り替えるブロックの種類
+  const paintBlockType = mc.MinecraftBlockTypes.wool;
+  // 塗り替え先ブロックの組み合わせ情報を作成する
+  const paintPermutation = createPaintPermutation(paintBlockType, colorName);
+  // 塗った色を数える
+  const score = new Score();
+
+  // 各座標別に繰り返し
+  for (let x = -radius; x <= radius; x += 1) {
+    for (let y = -radius; y <= radius; y += 1) {
+      for (let z = -radius; z <= radius; z += 1) {
+        // 当たったブロックから相対的な座標を取得する
+        const targetLocation = hitBlock.location.offset(x, y, z);
+        // その座標のブロックを取得する
+        const targetBlock = new Block(hitBlock.dimension.getBlock(targetLocation));
+        // そのブロックが塗り替え可能ならば
+        if (targetBlock.canPaint()) {
+          // 対象のブロック同じ種類のブロックなら
+          if (targetBlock.isSame(paintBlockType)) {
+            // 塗られて消えるブロックなので、その色の数を減らす
+            score.add(targetBlock.getColor(), -1);
+          }
+          // 対象ブロックを塗る
+          targetBlock.paint(paintBlockType, paintPermutation);
+          // 塗ったブロックなので、その色の数を増やす
+          score.add(colorName, 1);
+        }
+      }
+    }
+  }
+
+  // 塗り替えた色の数を保存する
+  score.save();
+}
+
+/**
+ * 羊に色を塗る
+ *
+ * @param entity
+ * @param colorName
+ * @returns
+ */
+function paintSheep(entity: mc.Entity, colorName: string) {
+  // 対象が羊であるか
+  if (entity.id !== mc.MinecraftEntityTypes.sheep.id) {
+    return;
+  }
+  // 色コンポーネントを持っているか（羊なら持っているはずだが一応）
+  if (!entity.hasComponent('minecraft:color')) {
+    return;
+  }
+  // 設定から色情報を取得する
+  const color = Setting.colors.find((v) => v.id === colorName);
+  if (color) {
+    // 色コンポーネントを取得して、値を変更
+    const colorComponent = entity.getComponent('minecraft:color') as mc.EntityColorComponent;
+    colorComponent.value = color.data;
+  }
+}
+
+/**
  * 発射物が当たった時のイベント
  *
  * @param event
  * @returns
  */
-export function projectileHit(event: mc.ProjectileHitEvent) {
+export function onProjectileHit(event: mc.ProjectileHitEvent) {
   // 発射したエンティティ
   const shooter = event.source;
   if (shooter.id !== mc.MinecraftEntityTypes.player.id) {
@@ -78,77 +138,55 @@ export function projectileHit(event: mc.ProjectileHitEvent) {
   if (projectile.id !== mc.MinecraftEntityTypes.snowball.id) {
     return;
   }
-
-  // ブロックに当たっていれば
-  const hitBlock: mc.BlockHitInformation = event.blockHit;
-  if (hitBlock !== undefined) {
-    // プレイヤーのタグから色を取得する
-    const colorName = new Player(shooter as mc.Player).getColorName();
-    // プレイヤーが色情報を持っていなければ終了
-    if (!colorName) {
-      return;
-    }
-
-    // 半径
-    const radius = 1;
-    // 塗り替えるブロックの種類
-    const paintBlockType = mc.MinecraftBlockTypes.wool;
-    // 塗り替え先ブロックの組み合わせ情報を作成する
-    const paintPermutation = getPaintPermutation(paintBlockType, colorName);
-    // 塗った色を数える
-    const score = new Score();
-
-    // 各座標別に繰り返し
-    for (let x = -radius; x <= radius; x += 1) {
-      for (let y = -radius; y <= radius; y += 1) {
-        for (let z = -radius; z <= radius; z += 1) {
-          // 当たったブロックから相対的な座標を取得する
-          const targetLocation = hitBlock.block.location.offset(x, y, z);
-          // その座標のブロックを取得する
-          const targetBlock = new Block(event.dimension.getBlock(targetLocation));
-          // そのブロックが塗り替え可能ならば
-          if (targetBlock.canPaint()) {
-            // 対象のブロック同じ種類のブロックなら
-            if (targetBlock.isSame(paintBlockType)) {
-              // 塗られて消えるブロックなので、その色の数を減らす
-              score.add(targetBlock.getColor(), -1);
-            }
-            // 対象ブロックを塗る
-            targetBlock.paint(paintBlockType, paintPermutation);
-            // 塗ったブロックなので、その色の数を増やす
-            score.add(colorName, 1);
-          }
-        }
-      }
-    }
-
-    // 塗り替えた色の数を保存する
-    score.save();
+  // プレイヤーから色情報を取得
+  const colorName = new Player(event.source as mc.Player).getColorName();
+  if (!colorName) {
+    return;
   }
 
+  // ブロックに当たっていれば色を変える
+  const hitBlock: mc.BlockHitInformation = event.blockHit;
+  if (hitBlock !== undefined) {
+    paintBlock(hitBlock.block, colorName);
+  }
   // 羊だったら色を変える
   const hitEntity: mc.EntityHitInformation = event.entityHit;
   if (hitEntity !== undefined) {
-    const entity = hitEntity.entity;
-    // 対象が羊であるか
-    if (hitEntity.entity.id !== mc.MinecraftEntityTypes.sheep.id) {
-      return;
+    paintSheep(hitEntity.entity, colorName);
+  }
+}
+
+/**
+ * 弾を補充する
+ */
+export function reloadBullet() {
+  const snowball = new mc.ItemStack(mc.MinecraftItemTypes.snowball, 1);
+  // すべてのプレイヤーで繰り返し
+  for (const mcPlayer of mc.world.getPlayers()) {
+    // スニークをしているかを判定
+    if (!mcPlayer.isSneaking) {
+      continue;
     }
-    // 色コンポーネントを持っているか
-    if (!entity.hasComponent('minecraft:color')) {
-      return;
+    const player = new Player(mcPlayer);
+    // 足元のブロックを確認
+    const bottomBlock = player.getBottomBlock();
+    if (!bottomBlock.isSame(mc.MinecraftBlockTypes.wool)) {
+      continue;
     }
-    // プレイヤーから色情報を取得できるか
-    const colorName = new Player(shooter as mc.Player).getColorName();
-    if (!colorName) {
-      return;
-    }
-    // 色情報を取得できるか
-    const color = Setting.colors.find((v) => v.id === colorName);
-    if (color) {
-      // 色コンポーネントを取得して、値を変更
-      const colorComponent = entity.getComponent('minecraft:color') as mc.EntityColorComponent;
-      colorComponent.value = color.data;
+    // そのプレイヤーの色と一致するか
+    const playerColor = player.getColorName();
+    if (playerColor && playerColor === bottomBlock.getColor()) {
+      // そのプレイヤーの座標にアイテムを作成(すぐに取得される)
+      mcPlayer.dimension.spawnItem(snowball, mcPlayer.location);
     }
   }
 }
+
+/** ブロックを壊したら雪玉 */
+// mc.world.events.blockBreak.subscribe((event) => {
+//   overworld.runCommand(`say ${event.brokenBlockPermutation.type.id}`);
+//   if (event.brokenBlockPermutation.type.id === mc.MinecraftBlockTypes.wool.id) {
+//     const snowball = new mc.ItemStack(mc.MinecraftItemTypes.snowball, 16);
+//     event.block.dimension.spawnItem(snowball, event.block.location);
+//   }
+// });
